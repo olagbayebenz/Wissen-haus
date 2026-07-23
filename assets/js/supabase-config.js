02fab2a45b1f0ce532c7a4dba25d5de44171deb4
Benz Olagbaye <bra@Benzs-MacBook-Pro.local>
2026-07-23 08:11:04 +0100

    Update Supabase API key with correct credentials

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -3,6 +3,6 @@
 
   window.supabase = createClient(
     'https://unahrcvjcxykrazdtixt.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuYWhyY3ZqY3h5a3JhemR0aXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0NTUxOTIsImV4cCI6MjA2NTAzMTE5Mn0.tHN2z5F9C9fZ6K8L9M3N4O5P6Q7R8S9T0U1V2W3X4Y'
+    'sb_publishable_QQ-q8Ly2qxnaiuF_tslPIA_m2mTJ19m'
   );
 })();
